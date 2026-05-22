837e1616eb2a2aa927e61bd442c9f64ebc886f57
qosuybixh4-sys <qosuybixh4@khu.ac.kr>
2026-05-22 14:52:28 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// 기존 서비스 워커 강제 제거
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.getRegistrations().then(registrations => {
     registrations.forEach(r => r.unregister());
